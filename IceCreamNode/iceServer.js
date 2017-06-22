3707bcebc5e43f30041cc7988280c1d929752647
Lisa <meisterl@hs-furtwangen.de>
2017-06-22 12:13:15 +0200

    scoop number validation fixed

--- IceCreamNode/iceServer.js
+++ IceCreamNode/iceServer.js
@@ -1,10 +1,32 @@
 "use strict";
 const http = require("http");
+const url = require("url");
+let port = process.env.PORT;
+if (port == undefined)
+    port = 8100;
 let server = http.createServer();
-server.addListener("listen", onListen);
-server.addListener("request", onRequest);
-server.listen(process.env.port || 8100); //nicht gefunden oder 8100
-function onListen() { }
-function onRequest(_request, _response) {
+server.addListener("listening", handleListen);
+server.addListener("request", handleRequest);
+server.listen(port);
+function handleListen() {
+    console.log("Server listening on port: " + process.env.port);
+}
+function handleRequest(_request, _response) {
+    console.log("Request recieved");
+    console.log(_request.url);
+    let query = url.parse(_request.url, true).query;
+    console.log(query);
+    let key;
+    for (key in query)
+        console.log(key + ":" + query[key]);
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.write("First Name: " + query["FirstName"] + "<br>");
+    _response.write("Last Name: " + query["LastName"]);
+    _response.end();
+    let data = {
+        nr: parseInt(url.parse(_request.url, true).query["nr"]),
+        selection: url.parse(_request.url, true).query["selection"]
+    };
 }
 //# sourceMappingURL=iceServer.js.map
--- IceCreamNode/iceServer.ts
+++ IceCreamNode/iceServer.ts
@@ -1,14 +1,54 @@
 import http = require("http");
 import url = require("url");
 
+interface AssocStringString {
+    [key: string]: string;
+}
+
+let port: number = process.env.PORT;
+if (port == undefined)
+    port = 8100;
+
+
 let server: http.Server = http.createServer();
-server.addListener("listen", onListen);
-server.addListener("request", onRequest);
-server.listen(process.env.port || 8100);  //nicht gefunden oder 8100
+server.addListener("listening", handleListen);
+server.addListener("request", handleRequest);
+server.listen(port);
 
 
-function onListen():void{}
+function handleListen(): void {
+    console.log("Server listening on port: " + process.env.port);
+}
 
-function onRequest(_request: http.IncomingMessage, _response: http.ServerResponse):void{
-    
+function handleRequest(_request: http.IncomingMessage, _response: http.ServerResponse): void {
+    console.log("Request recieved");
+    console.log(_request.url);
+
+    let query: AssocStringString = url.parse(_request.url, true).query;
+    console.log(query);
+    let key: string;
+    for (key in query)
+        console.log(key + ":" + query[key]);
+
+
+    _response.setHeader("Access-Control-Allow-Origin", "*");
+    _response.setHeader("content-type", "text/html; charset=utf-8");
+    _response.write("First Name: " + query["FirstName"] +  "<br>");
+    _response.write("Last Name: " + query["LastName"]);
+    _response.end();
+
+
+
+   
+    let data: MyData = {
+        nr: parseInt(url.parse(_request.url, true).query["nr"]),
+        selection: url.parse(_request.url, true).query["selection"]
+    };
+
+}
+
+
+interface MyData {
+    nr: number;
+    selection: string;
 }
